224e6ce704fc2728db601ee4d2e75d5f4c4cf130
Hoyeong Yoo <crystal23733@gmail.com>
2024-05-28 17:16:05 +0900

    module selector: 번호별 ID붙이기

--- public/js/modules/selector.js
+++ public/js/modules/selector.js
@@ -3,17 +3,16 @@ const thisWeekNumber = document.getElementById('this-week__number');
 
 const number = ['number1','number2','number3','number4','number5','number6','bonus'];
 
-const numberID = (week) => {
+// *각 번호의 배열 길이 순서와 id로 사용할 각 number순번이 같을 경우 id 할당
+export const numberID = (week) => {
   for(let i = 0; i < week.length; i++){
     for(let j = 0; j < number.length; j++){
       if(i === j){
         week[i].id = number[j];
-        console.dir(week[i]);
       } else if(i === 6){
         i++;
         j += 6;
         week[i].id = number[j];
-        console.dir(week[i]);
       } else if(i === 6){
         continue;
       }
