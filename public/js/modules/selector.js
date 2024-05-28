a5f4013c4ddbec38ef78d6e21e844921cadcb198
Hoyeong Yoo <crystal23733@gmail.com>
2024-05-28 17:26:42 +0900

    module: 기존 모듈 세분화

--- public/js/modules/selector.js
+++ public/js/modules/selector.js
@@ -1,25 +1,6 @@
 // *이번주 번호 선택자 / this-week
-const thisWeekNumber = document.getElementById('this-week__number');
+export const thisWeekNumber = document.getElementById('this-week__number');
 
-const number = ['number1','number2','number3','number4','number5','number6','bonus'];
 
-// *각 번호의 배열 길이 순서와 id로 사용할 각 number순번이 같을 경우 id 할당
-export const numberID = (week) => {
-  for(let i = 0; i < week.length; i++){
-    for(let j = 0; j < number.length; j++){
-      if(i === j){
-        week[i].id = number[j];
-      } else if(i === 6){
-        i++;
-        j += 6;
-        week[i].id = number[j];
-      } else if(i === 6){
-        continue;
-      }
-    }
-  } 
-}
 
-// *thisWeekNumber.children
 // todo 당첨번호별 색깔 핸들링을 위한 DOM API 선택자 만들기
-// todo 번호마다 id 붙이기
---
+++ public/js/thisWeek.js
@@ -0,0 +1,4 @@
+import { numberID } from "./modules/functions/numberID.js";
+import { thisWeekNumber } from "./modules/selector.js";
+
+numberID(thisWeekNumber.children);
