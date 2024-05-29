a753133e8759904d4db441b2f0a39e6d80419220
Hoyeong Yoo <crystal23733@gmail.com>
2024-05-30 00:05:31 +0900

    fix scss: numberColor 주석 추가

--- public/js/modules/functions/number/numberColor.js
+++ public/js/modules/functions/number/numberColor.js
@@ -1,8 +1,10 @@
+// todo 1~11...+10마다 색상 변경
+
 import { numberAll } from "../../selector.js";
 
 const numberColor = () => {
   numberAll.homeNumber.forEach((child) => {
-    console.log(child);
+    console.dir(child);
     const intText = parseInt(child.textContent);
     if(intText < 12){
       child.style.backgroudColor = '#FAC400';
