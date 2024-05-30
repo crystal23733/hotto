296fdb2399255bec2ffb0ed6922bb54488c82433
Hoyeong Yoo <crystal23733@gmail.com>
2024-05-30 10:10:47 +0900

    fix module: numberColor 모듈 완성

--- public/js/modules/functions/number/numberColor.js
+++ public/js/modules/functions/number/numberColor.js
@@ -1,15 +1,20 @@
-// todo 1~11...+10마다 색상 변경
+// *숫자별 색상 할당 함수
 
 import { numberAll } from "../../selector.js";
 
 const numberColor = () => {
   numberAll.homeNumber.forEach((child) => {
     const intText = parseInt(child.textContent);
-    if(intText < 12){
+    if(intText < 11){
       child.style.backgroundColor = '#FAC400';
+    } else if(intText < 21){
+      child.style.backgroundColor = '#69C8F2';
+    } else if(intText < 31){
+      child.style.backgroundColor = '#FF7171';
+    } else if(intText < 41){
+      child.style.backgroundColor = '#AAAAAA';
+    } else if(intText < 46){
+      child.style.backgroundColor = '#B0D840';
     }
-    console.dir(child);
   })
 }
-
-numberColor();
