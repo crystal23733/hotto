b4894dbfce1f3bf90d9cba033bc721054b53a17d
Hoyeong Yoo <crystal23733@gmail.com>
2024-05-31 10:27:12 +0900

    fix modules: 로그인 실패시 실행될 함수 추가중

--- public/js/join.js
+++
@@ -1,4 +0,0 @@
-import { joinValue } from "./modules/functions/join/joinHandle.js";
-import { joinForm } from "./modules/selector.js";
-
-joinForm.addEventListener('submit', joinValue);
--- public/js/modules/functions/join/joinHandle.js
+++ public/js/modules/functions/join/joinHandle.js
@@ -1,11 +1,12 @@
 // todo DB연결 전까지 회원정보 스토리지나 POST처리로 json파일에 담아놓기
 
 import { USERNAME_KEY } from "../../KEY.js";
-import { join } from "../../selector.js";
+import { join, joinForm } from "../../selector.js";
 import { conditional } from "./joinCondition.js";
 
-export const joinValue = (event) => {
-  event.preventDefault();
+// *회원가입 성공시 실행될 함수
+const joinSuccess = (event) => {
+  event.preventDefault(); // !함수 완성시 삭제
   // *회원 정보 오브젝트에 담기
   const joinObj = {
     name:join.name.value,
@@ -20,3 +21,13 @@ export const joinValue = (event) => {
   // *오브젝트를 배열에 담음
   localStorage.setItem(USERNAME_KEY, JSON.stringify(joinObj));
 }
+
+// *회원가입 실패시 실행될 함수
+const joinFail = (event) => {
+  event.preventDefault();
+  console.error('입력한 정보가 유효하지 않습니다.');
+  const p = document.createElement('p');
+  joinForm.appendChild(p);
+  p.textContent = '입력한 정보가 유효하지 않습니다.';
+}
+// joinForm.addEventListener('submit', joinSuccess);
