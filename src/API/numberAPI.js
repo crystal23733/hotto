38545889d495b61dafa31e2f35e7a833e9aea058
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-05 17:54:29 +0900

    fix API : for문으로 API를 긁어올 경우 한번에 많은 트래픽이 몰려 일정 시간마다 한번씩만 요청

--- src/API/numberAPI.js
+++ src/API/numberAPI.js
@@ -3,41 +3,23 @@ import fs from 'fs';
 import path from 'path';
 import { __dirname } from '../modules/findDirectory.js';
 
-// const numberAPI = () => {
-//   let time = 1122;
-//   for(let i = 1; i <= time; i++){
-//     let url = `https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=+${i}`;
-//     https.get(url, (res) => {
-//       let body = '';
-//       res.on('data', (data) => {
-//         body += data.toString();
-//         fs.writeFile(path.join(__dirname, `/public/js/API/historyNumber/history${i}.json`), body, (err) => {
-//           if(err){
-//             throw new Error('데이터를 생성할 수 없습니다.');
-//           }
-//           console.log('데이터를 성공적으로 생성하였습니다.');
-//         })
-//       });
-//     })
-//   }
-// }
+const numberAPI = () => {
+  let i = 1;
+  let url = `https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=+${i}`;
+  setInterval(() => {
+    https.get(url, (res) => {
+    let body = '';
+    res.on('data', (data) => {
+      body += data.toString();
+      fs.writeFile(path.join(__dirname, `/public/js/API/historyNumber/history${i}.json`), body, (err) => {
+        if(err){
+          throw new Error('데이터를 생성할 수 없습니다.');
+        }
+        console.log('데이터를 성공적으로 생성하였습니다.');
+      });
+    });
+  });
+  }, 15000);
+}
 
-// numberAPI();
-
-// const numberAPI = () => {
-//   let url = 'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=+1';
-//   https.get(url, (res) => {
-//     let body = '';
-//     res.on('data', (data) => {
-//       body += data.toString();
-//       // fs.writeFile(path.join(__dirname, '/public/js/API/historyNumber/history1.json'), body, (err) => {
-//       //   if(err){
-//       //     throw new Error('데이터를 생성할 수 없습니다.');
-//       //   }
-//       //   console.log('데이터를 성공적으로 생성하였습니다.');
-//       // })
-//     })
-//   })
-// }
-
-// numberAPI();
+numberAPI();
