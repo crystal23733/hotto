bc2ed521e070aba5ea9ca9e045e0aeb048926896
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-08 01:11:44 +0900

    생성완료

--- src/API/numberAPI.js
+++ src/API/numberAPI.js
@@ -6,8 +6,7 @@ import { __dirname } from '../modules/findDirectory.js';
 // !트래픽 초과 방지를 위해 일정한 시간마다 데이터 생성
 const numberAPI = () => {
   let i = 1;
-  const autoDataCreate = () => {
-    setInterval(() => {
+  const autoDataCreate = setInterval(() => {
       let url = `https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=+${i}`;
       console.log(i, url);
       https.get(url, (res) => {
@@ -19,16 +18,14 @@ const numberAPI = () => {
             throw new Error('데이터를 생성할 수 없습니다.');
           }
           console.log('데이터를 성공적으로 생성하였습니다.', i);
+          i++;
         });
       });
-      i++;
     });
-    }, 12000);
-  }
-  autoDataCreate();
-  if(i === 5){
-    clearInterval(autoDataCreate);
-  }
+    if(i === 1122){
+      clearInterval(autoDataCreate);
+    }
+  }, 7000);
 }
 
 numberAPI();
