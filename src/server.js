59a316b62c341d5015e53535799c384966c3a21b
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-10 17:40:40 +0900

    fix server:body를 파싱할 수 있도록 urlencoded추가

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ const app = express();
 
 // *css나 js같은 정적 파일을 폴더에서 전달할 수 있는지 확인하는 모든 수신 요청을 받아들이는 미들웨어. 파라미터로는 정적 파일에 대한 폴더 이름을 받는다.
 app.use(express.static('public'));
+app.use(express.urlencoded({extended : true}));
 
 app.use('/', rootRouter);
 
