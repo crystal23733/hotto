3c95b4fdc1809645a9afba10ceeead8e71d38548
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-10 17:31:41 +0900

    create scss: login style 생성, fix server: login 라우터 생성

--- src/controllers/rootController.js
+++ src/controllers/rootController.js
@@ -14,3 +14,12 @@ export const getJoin = (req, res) => {
 export const postJoin = (req, res) => {
   return res.status(302).redirect('/');
 }
+
+export const getLogin = (req, res) => {
+  const filePath = path.join(__dirname, 'public', 'login.html');
+  return res.status(200).sendFile(filePath);
+}
+
+export const postLogin = (req, res) => {
+  return res.status(302).redirect('/');
+}
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getHome, getJoin, postJoin } from '../controllers/rootController.js';
+import { getHome, getJoin, getLogin, postJoin, postLogin } from '../controllers/rootController.js';
 
 const rootRouter = express.Router();
 
 rootRouter.get('/', getHome);
 rootRouter.route('/join').get(getJoin).post(postJoin);
+rootRouter.route('/login').get(getLogin).post(postLogin);
 
 export default rootRouter;
