74518f433d9ba9e0b28b70998e739cda7e2ad196
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-12 15:00:36 +0900

    fix module: API의 폴더명이 바뀌어 수정

--- public/js/modules/functions/number/thisNumber.js
+++ public/js/modules/functions/number/thisNumber.js
@@ -1,16 +1,16 @@
-import data from '../../../API/historyNumber/history1123.json' with {type : 'json'};
+import data from '../../../API/history/history1123.json' with {type : 'json'};
 
 import { thisNumber } from '../../selectors/weekNumSelec.js';
-import { numberColor } from "./numberColor.js";
+import { numberColor } from './numberColor.js';
 
 // *최근 회차를 불러오는 함수
 export const thisNumText = () => {
-      thisNumber.one.textContent = data.drwtNo1;
-      thisNumber.two.textContent = data.drwtNo2;
-      thisNumber.three.textContent = data.drwtNo3;
-      thisNumber.four.textContent = data.drwtNo4;
-      thisNumber.five.textContent = data.drwtNo5;
-      thisNumber.six.textContent = data.drwtNo6;
-      thisNumber.bonus.textContent = data.bnusNo;
-      numberColor();
-}
+  thisNumber.one.textContent = data.drwtNo1;
+  thisNumber.two.textContent = data.drwtNo2;
+  thisNumber.three.textContent = data.drwtNo3;
+  thisNumber.four.textContent = data.drwtNo4;
+  thisNumber.five.textContent = data.drwtNo5;
+  thisNumber.six.textContent = data.drwtNo6;
+  thisNumber.bonus.textContent = data.bnusNo;
+  numberColor();
+};
