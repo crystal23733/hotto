6bded2917040ea5ff604ee29ef282fd9a937e2a4
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-12 15:46:32 +0900

    fix js: pick.js 번호 겹치지 않게 뽑는 함수 완료. 모듈화 필요

--- public/js/pick.js
+++ public/js/pick.js
@@ -2,19 +2,29 @@
 // todo 템플릿에 할당 해야함
 // *random번호를 뽑기위한 함수
 const randomNumber = () => {
+  // *1~45 번까지 저장하는 배열
   let number = [];
-  for(let i = 1; i <= 45; i++){
+  for (let i = 1; i <= 45; i++) {
     number.push(i);
   }
+  // *6개의 조합을 저장하는 배열
   let random = [];
-  for(let i = 1; i <= 6; i++){
-    if(random.includes(randomPick) === true){
-      let randomPick = Math.ceil(Math.random() * number.length);
-      random.push(randomPick);
-    }
+  // *random배열의 조합(길이)가 6개가 될 때까지 반복
+  while (random.length < 6) {
+    repPick(number, random);
+  }
+  console.log(random.sort());
+};
+
+// *조합이 겹칠 경우 뽑기 반복하는 함수
+const repPick = (number, random) => {
+  let randomPick = Math.ceil(Math.random() * number.length);
+  console.log(random.includes(randomPick));
+  if (random.includes(randomPick) === true) {
+    repPick(number, random);
+  } else {
     random.push(randomPick);
   }
-  console.log(random);
-}
+};
 
 randomNumber();
