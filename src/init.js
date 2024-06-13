a0de1b47065a6405e12d0eb2c30bec1b03da8238
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-13 12:00:50 +0900

    mongoDB 연결을 위한 mongoose 설치

--- src/init.js
+++ src/init.js
@@ -1,7 +1,8 @@
-import app from "./server.js";
+import 'dotenv/config';
+import app from './server.js';
 
 const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
-})
+});
