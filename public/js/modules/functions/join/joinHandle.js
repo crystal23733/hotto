a42a03df6988b2f2ad4aee68a9fb416c693bf915
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-13 12:44:08 +0900

    localStorage 삭제

--- public/js/modules/functions/join/joinHandle.js
+++ public/js/modules/functions/join/joinHandle.js
@@ -1,21 +1,16 @@
-// todo DB연결 전까지 회원정보 스토리지나 POST처리로 json파일에 담아놓기
-
-import { USERNAME_KEY } from "../../KEY.js";
-import { join } from "../../selectors/joinSelec.js";
+import { join } from '../../selectors/joinSelec.js';
 
 // *회원가입 성공시 실행될 함수
 export const joinSuccess = () => {
   // *회원 정보 오브젝트에 담기
   const joinObj = {
-    name:join.name.value,
-    email:join.email.value,
-    password:join.password.value,
-    checkPassword:join.checkPassword.value,
-    phone:join.phone.value
-  }
-  // *오브젝트를 배열에 담음
-  localStorage.setItem(USERNAME_KEY, JSON.stringify(joinObj));
-}
+    name: join.name.value,
+    email: join.email.value,
+    password: join.password.value,
+    checkPassword: join.checkPassword.value,
+    phone: join.phone.value,
+  };
+};
 
 // *회원가입 실패시 실행될 함수
 export const joinFail = (event) => {
@@ -25,4 +20,4 @@ export const joinFail = (event) => {
   joinForm.appendChild(p);
   p.style.color = 'red';
   p.textContent = '입력한 정보가 유효하지 않습니다.';
-}
+};
