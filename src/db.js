4b6785f181832344b6c7d6074982298a5ccbff7a
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-13 12:35:15 +0900

    DB 연결 성공

--- src/db.js
+++ src/db.js
@@ -1,1 +1,13 @@
-import { MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.DB_URL);
+
+const db = mongoose.connection;
+
+db.on('error', () => {
+  console.log('DB연결에 실패하였습니다.');
+});
+
+db.on('open', () => {
+  console.log('DB연결에 성공하였습니다.');
+});
