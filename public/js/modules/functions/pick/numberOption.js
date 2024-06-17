dec2f7140c34a6010f5759ae76ea4bf2ab61e924
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-17 23:35:24 +0900

    test: json파일 연속 읽기 테스트

--- public/js/modules/functions/pick/numberOption.js
+++ public/js/modules/functions/pick/numberOption.js
@@ -3,15 +3,17 @@ import path from 'path';
 import { __dirname } from '../../../../../src/modules/findDirectory.js';
 
 export const numberOption = () => {
-  let dataArr = [];
   for (let i = 1; i <= 1124; i++) {
+    let dataArr = [];
     const dataFile = fs.readFile(
-      path.join(__dirname, `public/js/API/history${i}`),
+      path.join(__dirname, `public/js/API/history/history${i}.json`),
       (err, data) => {
         if (err) {
-          console.error('파일을 읽어올 수 없습니다.');
+          console.error(err);
         }
-        console.log(data);
+        const jsonData = JSON.parse(data);
+        dataArr.push(jsonData);
+        console.log(dataArr);
       },
     );
   }
