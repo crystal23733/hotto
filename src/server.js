29a1693e0571dbadb55c0771d6c6a43cf6537843
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-05 17:53:01 +0900

    :recycle:static dist폴더 추가

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.set('views', process.cwd() + '/src/views');
 app.use(morgan('dev'));
 // *css나 js같은 정적 파일을 폴더에서 전달할 수 있는지 확인하는 모든 수신 요청을 받아들이는 미들웨어. 파라미터로는 정적 파일에 대한 폴더 이름을 받는다.
 app.use('/public', express.static('public'));
-app.use('/src', express.static('src'));
+app.use(express.static('dist'));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', rootRouter);
