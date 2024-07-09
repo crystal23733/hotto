597fef66980cda65b2b21ccebc5088c7b81359f4
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-09 21:48:23 +0900

    :recycle:joinSelec 타입선언 인터페이스 추가

---
+++ src/client/ts/modules/selectors/interface/JoinInfo.ts
@@ -0,0 +1,9 @@
+interface JoinInfo {
+  name: HTMLElement | null;
+  email:HTMLElement | null;
+  password: HTMLElement | null;
+  checkPassword: HTMLElement | null;
+  phone: HTMLElement | null;
+}
+
+export default JoinInfo;
