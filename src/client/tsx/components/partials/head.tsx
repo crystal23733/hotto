790c208883bfe7776b57eb7276dbbf02c829337a
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-16 15:26:51 +0900

    :art:레이아웃 컴포넌트 생성

--- src/client/tsx/components/partials/head.tsx
+++ src/client/tsx/components/partials/head.tsx
@@ -3,16 +3,18 @@ import { Helmet } from "react-helmet-async";
 
 interface HeadProps {
   pageTitle : string;
+  scriptSrc : string;
 }
 
-const Head:React.FC<HeadProps> = ({pageTitle}) => {
+const Head:React.FC<HeadProps> = ({pageTitle, scriptSrc}) => {
   return (
   <Helmet>
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{`hotto | ${pageTitle}`}</title>
+    <script src={scriptSrc}></script>
   </Helmet>
   )
 }
 
-export default Helmet;
+export default Head;
--- src/client/tsx/layout.tsx
+++ src/client/tsx/layout.tsx
@@ -1,1 +1,22 @@
-import React from "react";
+import React from 'react';
+import Head from './components/partials/head';
+import Header from './components/partials/header';
+import Menu from './components/partials/menu';
+
+const Layout = ({ children, pageTitle, scriptSrc }) => {
+  return (
+    <html lang="kr">
+      <Head pageTitle={pageTitle} scriptSrc={scriptSrc} />
+      <body>
+        <Header />
+        <Menu />
+        <div id="content">
+          {children}
+        </div>
+        <script src="your-script.js"></script>
+      </body>
+    </html>
+  );
+};
+
+export default Layout;
