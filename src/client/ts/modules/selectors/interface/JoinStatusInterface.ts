d056465fd51884ffcecef2594ea41c64b0614119
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-18 09:23:49 +0900

    :truck:클라이언트 코드 이동

--- src/client/ts/modules/selectors/interface/JoinStatusInterface.ts
+++
@@ -1,9 +0,0 @@
-interface JoinStatusInterface {
-  name: HTMLElement | null;
-  email: HTMLElement | null;
-  password: HTMLElement | null;
-  checkPassword: HTMLElement | null;
-  phone: HTMLElement | null;
-}
-
-export default JoinStatusInterface;
