6e4edb1d70e5fcf7c8b8e75e0294161bd329531d
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-19 15:27:31 +0900

    :heavy_plus_sign:layout.tsx 수정

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import React, {ReactNode} from "react";
+import BasicTextProps from "./components/interface/basicTextProps";
+import Head from "./components/customHead";
+import Header from "./components/header";
+import Menu from "./components/menu";
+
+interface LayoutProps extends BasicTextProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children, pageTitle, scriptText }) => {
+  return (
+    <>
+      <Head pageTitle={pageTitle} scriptText={scriptText} />
+      <body>
+        <Header />
+        <Menu />
+        {children}
+      </body>
+    </>
+  );
+}
+
+export default Layout;
