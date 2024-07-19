03d0062def5ea83303f3a8dd6cd92245455ead2e
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-20 01:33:10 +0900

    :art:유호영:주간 당첨번호 할당

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,57 @@
-import React from "react"
+import React, { useState, useEffect, useRef } from "react"
 import Layout from "../app/layout";
 
+interface Data {
+  drwtNo1: number;
+  drwtNo2: number;
+  drwtNo3: number;
+  drwtNo4: number;
+  drwtNo5: number;
+  drwtNo6: number;
+  bnusNo: number;
+}
+
 const Home:React.FC = () => {
+  const [numbers, setNumbers] = useState<(number | string)[]>([]);
+  const numberRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  // * 주간 당첨번호를 정적파일로 불러오는 함수
+  useEffect(() => {
+    const weekNumData = async () => {
+      try{
+        const response = await fetch('/API/history/history1128.json');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data: Data = await response.json();
+        console.log(data);
+        const fetchedNumbers: (number | string)[] = [
+          data.drwtNo1,
+          data.drwtNo2,
+          data.drwtNo3,
+          data.drwtNo4,
+          data.drwtNo5,
+          data.drwtNo6,
+          '+',
+          data.bnusNo
+        ];
+        setNumbers(fetchedNumbers);
+      } catch(error){
+        console.error('Error fetching data:', error);
+      }
+    }
+    weekNumData();
+  }, []);
+
+  // * number에 id를 담는 함수
+  useEffect(() => {
+    numberRefs.current.forEach((element, index) => {
+      if(element){
+        element.id = `number${index + 1}`;
+      }
+    })
+  }, [numbers]);
+
   return (
     <Layout pageTitle="Home">
       <div id="this-week">
@@ -9,16 +59,14 @@ const Home:React.FC = () => {
           <h1>금주 당첨번호</h1>
         </div>
         <div id="this-week__number">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <p>
-            +
-          </p>
-          <div></div>
+          {numbers.map((number, index) => (
+            <div key={index} ref={el => {
+              // 올바른 타입으로 ref 설정
+              if (el) numberRefs.current[index] = el;
+            }}>
+              {number}
+            </div>
+          ))}
         </div>
       </div>
       <div id="content-box">
