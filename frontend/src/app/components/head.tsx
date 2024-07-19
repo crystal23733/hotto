aebc55a5c3a347f2c2c29260eb5972ce0f735c41
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-19 12:02:30 +0900

    :art:Menu컴포넌트 생성

--- frontend/src/app/components/head.tsx
+++ frontend/src/app/components/head.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 interface HeadProps {
   pageTitle: string;
+  scriptText : string;
 }
 
-const Head: React.FC<HeadProps> = ({ pageTitle }) => {
+const Head: React.FC<HeadProps> = ({ pageTitle, scriptText }) => {
   return (
     <head>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <script src="https://kit.fontawesome.com/8809ab29cc.js" crossOrigin="anonymous"></script>
       <title>hotto | {pageTitle}</title>
+      <script src={scriptText}></script>
     </head>
   );
 }
