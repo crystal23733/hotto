fbc51e0e2f4564a6444d9f026a7e19fc6a44b4dc
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-22 23:38:25 +0900

    :recycle:cors, morgan추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import morgan from 'morgan';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const PORT = process.env.PORT || 3001;
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET, POST, PUT, PATCH, HEAD, DELETE', 
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+  app.use(morgan('dev'));
   await app.listen(PORT);
   console.log(`http://localhost:${PORT}`);
 }
