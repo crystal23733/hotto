2cc9c6b63f50bb16c4f7b320f9a62f4338581c61
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-23 23:50:22 +0900

    :recycle:root controller 일부 함수 리팩토링

--- server/src/root/root.service.ts
+++ server/src/root/root.service.ts
@@ -1,2 +1,30 @@
 import { Injectable } from "@nestjs/common";
-import User from "@database/src/model/userModel";
+import User from "@database/model/userModel";
+import { conditional } from "@shared/functions/joinCondition";
+
+@Injectable()
+export class RootService {
+  async handleJoin(body) {
+    const { name, email, password, checkPassword, phone} = body;
+    if(!conditional.emailCon(email)) {
+      return { error: '이메일 양식이 유효하지 않습니다.'};
+    }
+    if(password !== checkPassword){
+      return { error: '비밀번호가 동일하지 않습니다.'};
+    }
+    if(!conditional.phoneCon){
+      return { error: '전화번호 양식이 유효하지 않습니다.'};
+    }
+    const existsName = await User.exists({ name }); // 사용자 이름 존재 확인
+    const existsEmail = await User.exists({ email }); // 이메일 존재 확인
+    const existsPhone = await User.exists({ phone }); // 전화번호 존재 확인
+    try {
+      await User.create({ name, email, password, phone }); // 새 사용자 생성
+      return { message: 'User created successfully' };
+    } catch (error) {
+      if (existsName) return { error: '아이디가 이미 존재합니다.' };
+      if (existsEmail) return { error: '이메일이 이미 존재합니다.' };
+      if (existsPhone) return { error: '전화번호가 이미 존재합니다.' };
+    }
+  }
+}
