b0f06dc22864e05c07697a8e479ddc1ac83f226c
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-23 16:59:38 +0900

    :heavy_plus_sign:main.ts dotenv추가

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import morgan from 'morgan';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import morgan from "morgan";
+import "dotenv/config";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const PORT = process.env.PORT || 3001;
   app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET, POST, PUT, PATCH, HEAD, DELETE', 
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: "http://localhost:3000",
+    methods: "GET, POST, PUT, PATCH, HEAD, DELETE",
+    allowedHeaders: "Content-Type, Authorization",
   });
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
   await app.listen(PORT);
   console.log(`http://localhost:${PORT}`);
 }
--- server/src/root/root.service.ts
+++ server/src/root/root.service.ts
@@ -1,3 +1,2 @@
 import { Injectable } from "@nestjs/common";
-import User from '@database/src/model/userModel';
-
+import User from "@database/src/model/userModel";
