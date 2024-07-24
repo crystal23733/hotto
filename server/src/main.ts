bb362f3ada006844688fcbd4fe52ad11a1ffbc11
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-24 23:30:14 +0900

    :recycle:root router 생성

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import morgan from "morgan";
-import "dotenv/config";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
