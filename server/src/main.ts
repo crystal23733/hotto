94fd40b7d76e2a51d6ac4096e6745661f68bb5cb
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-29 17:49:22 +0900

    :memo:CORS URI 명시

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   // CORS 설정
   app.enableCors({
-    origin: "*",
+    origin: "http://localhost:3001",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type, Authorization",
   });
