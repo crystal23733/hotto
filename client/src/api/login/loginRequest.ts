d09826e01520e498b9b9d72fe3280baf9a7e7589
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-31 00:31:05 +0900

    :sparkle:로그인 데이터 통신을 위한 함수 loginRequest 추가

---
+++ client/src/api/login/loginRequest.ts
@@ -0,0 +1,24 @@
+/**
+ * * 로그인데이터
+ * @param email
+ * @param password
+ * @return response.json
+ */
+export default async (email: string, password: string) => {
+  try {
+    const response = await fetch("http://localhost:8080", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({ email, password })
+    })
+    if (!response.ok) {
+      throw new Error;
+    }
+
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+}
