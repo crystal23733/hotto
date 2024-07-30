94605bae1e662076fd1fe34c47a2602f7d1c7dbc
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-31 00:40:30 +0900

    :truck:joinCondition -> condition 이름 변경

--- server/src/root/root.service.ts
+++ server/src/root/root.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from "@nestjs/common";
-import { conditional } from "@shared/pipes/joinCondition";
+import { conditional } from "@shared/pipes/condition";
 import { User } from "../database/model/user.schema";
 import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
 
 @Injectable()
 export class RootService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  constructor(@InjectModel(User.name) private userModel: Model<User>) { }
   async handleJoin(body) {
     const { name, email, password, checkPassword, phone } = body;
     if (!conditional.emailCon(email)) {
