bfe5db4aaac08f35cde29048da31daf63aeafa80
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-30 22:10:59 +0900

    :sparkles:cors 쿠키 허용

--- server/src/main.ts
+++ server/src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     origin: "http://localhost:3000",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type, Authorization",
+    credentials: true
   });
 
   // 로깅 미들웨어 설정
