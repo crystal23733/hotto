749eee25ee3a66523540ef63e2bb3158b221acb6
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-02 00:01:38 +0900

    :package:세션 및 쿠키 미들웨어 모듈화

---
+++ server/src/common/middleware/session.middleware.ts
@@ -0,0 +1,32 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import MongoStore from "connect-mongo";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+
+@Injectable()
+export class SessionMiddleware implements NestMiddleware {
+  constructor(private configService: ConfigService) { }
+
+  use(req: any, res: any, next: (error?: Error | any) => void) {
+    cookieParser()(req, res, (err) => {
+      if (err) {
+        return next(err);
+      }
+      session({
+        secret: this.configService.get<string>("SECRET_KEY")!,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+          httpOnly: true,
+          maxAge: this.configService.get<number>("SECRET_AGE") || 86400000,
+          sameSite: "strict",
+        },
+        store: MongoStore.create({
+          mongoUrl: this.configService.get<string>("DB_URL"),
+          collectionName: "sessions",
+        }),
+      })(req, res, next);
+    })
+  }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-import session from "express-session";
-import MongoStore from "connect-mongo";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,30 +8,6 @@ async function bootstrap() {
   // ! 환경변수 관리를 위한 설정
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>("PORT") || 3000;
-  const DB_Url = configService.get<string>("DB_URL");
-  const cookieSecretKey = configService.get<string>("SECRET_KEY")!;
-  const cookieAge = configService.get<number>("SECRET_AGE") || 86400000;
-
-  // * 쿠키를 파싱하기 위한 미들웨어
-  app.use(cookieParser());
-  // * express를 위한 미들웨어
-  // todo 추후 https 인증 secure:true 추가 필요
-  app.use(
-    session({
-      secret: cookieSecretKey,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-        maxAge: cookieAge,
-        sameSite: "strict",
-      },
-      store: MongoStore.create({
-        mongoUrl: DB_Url,
-        collectionName: "sessions",
-      }),
-    }),
-  );
 
   // CORS 설정
   app.enableCors({
@@ -44,9 +16,6 @@ async function bootstrap() {
     allowedHeaders: "Content-Type, Authorization",
     credentials: true,
   });
-
-  // 로깅 미들웨어 설정
-  app.use(morgan("dev"));
 
   await app.listen(PORT);
   console.log(`http://localhost:${PORT}`);
