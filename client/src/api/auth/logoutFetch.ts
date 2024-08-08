fab208bff16299cc4f4dabf80e8975066f173f0f
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-09 01:21:01 +0900

    :truck::recycle:로그아웃 api 요청 함수 리팩토링

--- client/src/api/auth/logoutFetch.ts
+++ client/src/api/auth/logoutFetch.ts
@@ -1,17 +1,11 @@
+import FetchApi from "../lib/FetchApi";
+
+const fetchApi = new FetchApi("http://localhost:8080");
+
 /**
  * 24.08.06
  * @returns {response} - 로그아웃 요청
  */
 export default async () => {
-  try {
-    const response = await fetch("http://localhost:8080/auth/logout", {
-      method: "POST",
-      credentials: "include",
-    });
-    if (!response.ok) {
-      throw new Error("로그아웃 요청이 실패했습니다");
-    }
-  } catch (error) {
-    console.error("로그아웃 중 오류 발생:", error);
-  }
+  return await fetchApi.request("/auth/logout", "POST", undefined, undefined, true);
 };
