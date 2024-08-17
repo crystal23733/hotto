ea555c48e583f8fdc79069d6e426fe27f736f5bd
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-18 01:34:43 +0900

    :package:로또 당첨번호 가져오기

--- client/src/components/pick/OptionSelector.tsx
+++ client/src/components/pick/OptionSelector.tsx
@@ -9,6 +9,7 @@ const OptionSelector: React.FC<OptionSelectorProps> = ({
   selectedOption,
   onChange,
 }) => {
+  const unique = process.env.NEXT_PUBLIC_API_UNIQUE_ENDPOINT as string;
   return (
     <div id="options">
       <label htmlFor="lottoOption">번호 뽑기 옵션:</label>
@@ -16,7 +17,7 @@ const OptionSelector: React.FC<OptionSelectorProps> = ({
         <option value="default">기본 번호 뽑기 (무료)</option>
         <option value="even">짝수 번호 뽑기 (유료)</option>
         <option value="multipleOfFive">5의 배수 번호 뽑기 (유료)</option>
-        <option value="noHistory">역대 당첨 기록 없는 번호 뽑기 (유료)</option>
+        <option value={unique}>역대 당첨 기록 없는 번호 뽑기 (유료)</option>
       </select>
     </div>
   );
--- client/src/hook/pick/useGenerateNumbers.ts
+++ client/src/hook/pick/useGenerateNumbers.ts
@@ -1,4 +1,5 @@
 import randomNumber from "@shared/utils/randomNumber";
+import lottoApi from "client/src/api/content/lottoApi";
 import setNumberBgColor from "client/src/utils/setNumberBgColor";
 import { useState, useEffect, useRef } from "react";
 
@@ -8,21 +9,38 @@ import { useState, useEffect, useRef } from "react";
  * 이 훅은 1부터 45까지의 숫자 중 무작위로 6개의 숫자를 생성합니다. 생성된 숫자는
  * 상태로 저장되며, `numberListRef`를 통해 참조된 HTML 요소에 대해 숫자를 표시하고,
  * 배경 색상을 설정합니다.
- *
+ * todo 유료 번호 생성 로직 이용시 결제
+ * todo 유료 번호 생성 실패시 환불
  * @returns {{
- *   numbers: number[],            // 현재 생성된 숫자 배열입니다.
- *   numberListRef: React.RefObject<HTMLDivElement>, // 숫자 목록의 참조를 담고 있는 Ref 객체입니다.
- *   generateNumbers: () => void   // 숫자를 생성하는 함수입니다.
- * }}
+*   numbers: number[],            // 현재 생성된 숫자 배열입니다.
+*   numberListRef: React.RefObject<HTMLDivElement>, // 숫자 목록의 참조를 담고 있는 Ref 객체입니다.
+*   generateNumbers: (option: string) => Promise<void>   // 선택된 옵션에 따라 번호를 생성하는 함수입니다.
+* }}
  *
  * @date 24.08.08
  */
 const useGenerateNumbers = () => {
   const [numbers, setNumbers] = useState<number[]>([]);
+  const [error, setError] = useState<string | null>(null);
   const numberListRef = useRef<HTMLDivElement>(null);
 
-  const generateNumbers = () => {
-    setNumbers(randomNumber);
+  const generateNumbers = async (option: string) => {
+    setError(null);
+    if (option === 'default') {
+      setNumbers(randomNumber);
+    } else {
+      try {
+        const fetchedNumbers = await lottoApi(option);
+        console.log(fetchedNumbers);
+        setNumbers(fetchedNumbers);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError("특수 번호 생성에 실패했습니다. " + error.message);  // 오류 메시지 설정
+        } else {
+          setError("특수 번호 생성에 실패했습니다.");  // 일반적인 오류 메시지 설정
+        }
+      }
+    }
   };
 
   useEffect(() => {
@@ -42,7 +60,7 @@ const useGenerateNumbers = () => {
     }
   }, [numbers]);
 
-  return { numbers, numberListRef, generateNumbers };
+  return { numbers, numberListRef, generateNumbers, error };
 };
 
 export default useGenerateNumbers;
