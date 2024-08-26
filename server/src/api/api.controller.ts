cc2402268539a0e0fe16088ba8f92804c127718f
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-27 00:36:17 +0900

    :sparkles:특정 회차차의 로또 데이터를 불러오는 컨트롤러 생성

--- server/src/api/api.controller.ts
+++ server/src/api/api.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiService } from './api.service';
-import LottoData from '../../../shared/interface/lotto-data.interface';
+import { Controller, Get, Param } from "@nestjs/common";
+import { ApiService } from "./api.service";
+import LottoData from "../../../shared/interface/lotto-data.interface";
 
 /**
  * ApiController는 API 엔드포인트를 관리하는 컨트롤러입니다.
  * 고유한 로또 번호 조합과 최신 로또 데이터를 제공하는 엔드포인트를 정의합니다.
  */
-@Controller('api')
+@Controller("api")
 export class ApiController {
   constructor(private readonly apiService: ApiService) {}
 
@@ -15,7 +15,7 @@ export class ApiController {
    *
    * @returns {Promise<number[]>} - 고유한 로또 번호 조합
    */
-  @Get('unique-number')
+  @Get("unique-number")
   async getLottoData(): Promise<number[]> {
     return this.apiService.getLottoData();
   }
@@ -25,8 +25,20 @@ export class ApiController {
    *
    * @returns {Promise<LottoData>} - 최신 회차의 로또 데이터
    */
-  @Get('latest-numbers')
+  @Get("latest-numbers")
   async getLatestLottoData(): Promise<LottoData> {
     return this.apiService.getLatestLottoData();
+  }
+
+  /**
+   * 특정 회차의 로또 데이터를 반환합니다.
+   *
+   * @param {string} round - 회차 번호
+   * @returns {Promise<LottoData>} - 로또 데이터
+   */
+  @Get(":round")
+  async getLottoDataRound(@Param("round") round: string): Promise<LottoData> {
+    // 서비스에서 회차에 맞는 데이터 조회 로직 추가 필요
+    return this.apiService.getLottoDataForRound(round);
   }
 }
