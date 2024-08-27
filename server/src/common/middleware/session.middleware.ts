9a8d177d60d7271c12db05b1ebccd8a4121fa2bb
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-28 02:09:25 +0900

    :package:세션 쿠키 설정 변경
    sameSite:strict -> lax변경 - 같은 도메인만 허용
    secure:isProduction - 배포 환경일 경우 https통신만 허용

--- server/src/common/middleware/session.middleware.ts
+++ server/src/common/middleware/session.middleware.ts
@@ -10,11 +10,12 @@ export class SessionMiddleware implements NestMiddleware {
 
   use(req: any, res: any, next: (error?: Error | any) => void) {
     // * 환경에 따른 secure 설정
-    const isProduction = process.env.NODE_ENV === 'production';
+    const isProduction = process.env.NODE_ENV === "production";
     cookieParser()(req, res, (cookieErr) => {
       if (cookieErr) {
         return next(cookieErr);
       }
+      const DB_URL = this.configService.get<string>("DB_URL")!
       session({
         secret: this.configService.get<string>("SECRET_KEY")!,
         resave: false,
@@ -24,11 +25,11 @@ export class SessionMiddleware implements NestMiddleware {
           httpOnly: true,
           maxAge:
             Number(this.configService.get<number>("SECRET_AGE")) || 86400000,
-          sameSite: isProduction ? "none" : "strict",
+          sameSite: "lax",
           secure: isProduction
         },
         store: MongoStore.create({
-          mongoUrl: this.configService.get<string>("DB_URL"),
+          mongoUrl: DB_URL,
           collectionName: "sessions",
           autoRemove: "native", // MongoDB의 TTL 인덱스 사용
           touchAfter: 24 * 3600, // 24시간마다 세션 업데이트
