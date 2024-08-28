1adf4f2d428ac2e075841c05c6a4afdf6a5b0b92
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-28 16:04:34 +0900

    :package:로그아웃시 리로드 -> 리디렉션으로 변경

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -17,7 +17,7 @@ const Header: React.FC = () => {
     const response = await checkAuthStatue();
     if (response.isAuthenticated) {
       setIsAuthenticated(false);
-      router.reload();
+      router.push("/");
     }
   };
 
