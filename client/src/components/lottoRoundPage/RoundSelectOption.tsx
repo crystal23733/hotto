de472c280405a1cb3c13ca7180964faf23a5e9ef
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-29 15:31:26 +0900

    :sparkles:회차 선택 컴포넌트
    회차별로 선택하여 데이터를 볼 수 있는 컴포넌트 추가

---
+++ client/src/components/lottoRoundPage/RoundSelectOption.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+/**
+ * LottoRoundSelect 컴포넌트에 전달되는 props 인터페이스
+ * @typedef {Object} LottoRoundSelectProps
+ * @property {function} onSelectRound - 회차 선택 시 호출되는 콜백 함수
+ * @property {number} maxRound - 선택할 수 있는 최대 회차 번호
+ */
+interface IRoundSelectOption {
+  onSelectRound: (round: string) => void;
+  maxRound: number;
+}
+
+/**
+ * LottoRoundSelect 컴포넌트
+ *
+ * 사용자가 특정 회차를 선택할 수 있는 드롭다운 메뉴를 제공하는 컴포넌트입니다.
+ * maxRound 값에 따라 옵션을 동적으로 생성하며, 회차가 선택되면 onSelectRound 콜백 함수가 호출됩니다.
+ *
+ * @component
+ * @param {LottoRoundSelectProps} props - 컴포넌트에 전달되는 props
+ * @returns {JSX.Element} LottoRoundSelect 컴포넌트
+ * @example
+ * return (
+ *   <LottoRoundSelect onSelectRound={(round) => console.log(round)} maxRound={1134} />
+ * )
+ */
+const RoundSelectOption: React.FC<IRoundSelectOption> = ({
+  onSelectRound,
+  maxRound,
+}) => {
+  const [selectRound, setSelectRound] = useState<string>("");
+
+  /**
+   * handleChange - 사용자가 드롭다운에서 다른 회차를 선택할 때 호출되는 함수
+   *
+   * @param {React.ChangeEvent<HTMLSelectElement>} e - 드롭다운에서 발생한 change 이벤트 객체
+   */
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = e.target.value;
+    setSelectRound(selectedValue);
+    onSelectRound(selectedValue);
+  };
+  return (
+    <>
+      <label htmlFor="lottoRoundSelect">회차 선택</label>
+      <div className="select">
+        <select
+          id="lottoRoundSelect"
+          value={selectRound}
+          onChange={handleChange}
+        >
+          <option value="">회차를 선택하세요.</option>
+          {[...Array(maxRound)].map((_, index) => {
+            const roundNumber = maxRound - index; // * 최신회차가 상단에 오도록 설정
+            return (
+              <option value={`history${roundNumber}`} key={roundNumber}>
+                {roundNumber} 회차
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </>
+  );
+};
+
+export default RoundSelectOption;
