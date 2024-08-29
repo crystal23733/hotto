7c073125f1177b4e5bc3122ef9a9ea0526c37af0
Hoyeong Yoo <crystal23733@gmail.com>
2024-08-29 15:31:16 +0900

    :bug:useLottoRound 파일 async부분 수정

--- client/src/hook/LottoRoundPage/useLottoRound.ts
+++ client/src/hook/LottoRoundPage/useLottoRound.ts
@@ -8,7 +8,7 @@ import ILottoRoundData from "@shared/interface/lottoRound.interface";
  * @param {string} round - 조회할 로또 회차
  * @returns {{data: LottoData | null, loading: boolean, error: Error | null}} 로또 데이터, 로딩 상태, 에러 상태
  */
-export default async (round: string) => {
+export default (round: string) => {
   const { data, loading, error, setData, setLoading, setError } =
     useApiRequest<ILottoRoundData>();
 
