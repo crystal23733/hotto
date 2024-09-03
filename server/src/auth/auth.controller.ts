3016b0068b609b4f687be5394d9f15cfa07eac94
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-03 23:13:22 +0900

    :sparkles:백엔드 측 인증 서비스 로직 추가
    클라이언트 측 비밀번호를 확인해주는 로직 추가

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import {
   Post,
   Req,
   Res,
+  UnauthorizedException,
 } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { Request, Response } from "express";
@@ -54,6 +55,49 @@ export class AuthController {
       return res.json({ message: "로그아웃 성공" });
     } catch (error) {
       return res.status(500).json({ message: "로그아웃 실패" });
+    }
+  }
+
+  /**
+   * 사용자가 입력한 비밀번호를 검증하는 API 엔드포인트입니다.
+   * @param {string} password - 사용자가 입력한 비밀번호
+   * @param {Request} req - Express 요청 객체 (세션을 포함)
+   * @param {Response} res - Express 응답 객체
+   * @returns {Promise<Response>} - 비밀번호 검증 결과를 응답합니다.
+   * @throws {UnauthorizedException} - 세션이 만료되었거나 존재하지 않을 경우 예외를 발생시킵니다.
+   */
+  @Post("verify-password")
+  async verifyPassword(
+    @Body("password") password: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<Response> {
+    try {
+      const userId = req.session.userId;
+      if (!userId) {
+        throw new UnauthorizedException(
+          "세션이 만료되었거나 존재하지 않습니다.",
+        );
+      }
+      const result = await this.authService.verifyPassword(userId, password);
+      if (result.success) {
+        return res.status(HttpStatus.OK).json({ success: true });
+      } else {
+        return res
+          .status(HttpStatus.UNAUTHORIZED)
+          .json({ success: false, message: result.message });
+      }
+    } catch (error) {
+      if (error instanceof UnauthorizedException) {
+        return res
+          .status(HttpStatus.UNAUTHORIZED)
+          .json({ success: false, message: error.message });
+      } else {
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+          success: false,
+          message: "비밀번호 검사중 알 수 없는 오류가 발생하였습니다.",
+        });
+      }
     }
   }
 }
