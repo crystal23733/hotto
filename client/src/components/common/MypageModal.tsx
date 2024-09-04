12fe6a3c8d4d1774505e1558f5f138843f6a59bf
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-04 23:46:32 +0900

    :bulb::package:MypageModal컴포넌트 로직 수정 및 주석
    JSDoc추가
    커스텀 훅 변경에 따른 로직 변경

--- client/src/components/common/MypageModal.tsx
+++ client/src/components/common/MypageModal.tsx
@@ -1,5 +1,4 @@
 import useMypageModal from "client/src/hook/common/useMypageModal";
-import { useRouter } from "next/router";
 import React from "react";
 
 interface MypageModalProps {
@@ -7,18 +6,37 @@ interface MypageModalProps {
   closeModal: () => void;
 }
 
+/**
+ * MypageModal 컴포넌트
+ *
+ * 사용자가 마이페이지로 진입하기 전 비밀번호를 확인하는 모달 컴포넌트입니다.
+ * 비밀번호를 입력하고 "마이페이지" 버튼을 클릭하면, 비밀번호 검증 후 마이페이지로 이동합니다.
+ *
+ * @component
+ *
+ * @param {MypageModalProps} props - 컴포넌트에 전달되는 props
+ * @param {boolean} props.isActive - 모달의 활성화 상태를 나타냅니다. true면 모달이 활성화됩니다.
+ * @param {function} props.closeModal - 모달을 닫는 함수입니다.
+ *
+ * @returns {React.FC} MypageModal 컴포넌트
+ */
 const MypageModal: React.FC<MypageModalProps> = ({ isActive, closeModal }) => {
-  const { password, setPassword, data, loading, error, verifyPassword } =
+  const { password, setPassword, error, loading, verifyPasswordAndNavigate } =
     useMypageModal();
-  const router = useRouter();
 
-  const handleMypageBtn = async () => {
-    await verifyPassword();
-    console.log(data);
-
-    if (data?.success) {
-      router.push("/Mypage");
-      closeModal();
+  /**
+   * "마이페이지" 버튼 클릭 시 호출되는 함수입니다.
+   * 비밀번호 검증을 수행하고, 성공 시 마이페이지로 이동합니다.
+   *
+   * @param {React.MouseEvent<HTMLButtonElement>} event - 클릭 이벤트
+   */
+  const handleMypageBtn = async (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    event.preventDefault();
+    const success = await verifyPasswordAndNavigate(); // 비밀번호 검증 및 페이지 이동을 처리하고 성공 여부 반환
+    if (success) {
+      closeModal(); // 성공 시 모달 닫기
     }
   };
 
@@ -46,7 +64,11 @@ const MypageModal: React.FC<MypageModalProps> = ({ isActive, closeModal }) => {
         </section>
         <footer className="modal-card-foot">
           <div className="buttons">
-            <button className="button is-success" onClick={handleMypageBtn}>
+            <button
+              className="button is-success"
+              onClick={handleMypageBtn}
+              disabled={loading}
+            >
               마이페이지
             </button>
             <button className="button" type="reset" onClick={closeModal}>
