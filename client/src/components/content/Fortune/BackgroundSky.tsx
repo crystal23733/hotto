1471747dfaf77d2ec9c7d44fab1f1853c7e9d941
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-19 14:24:25 +0900

    :art:별 산개 및 이동 거리 증가

--- client/src/components/content/Fortune/BackgroundSky.tsx
+++ client/src/components/content/Fortune/BackgroundSky.tsx
@@ -3,7 +3,7 @@ import "../../../scss/fortune.scss";
 
 const BackgroundSky: React.FC = () => {
   return (
-    <div id="background">
+    <div id="background-box">
       <div className="night">
         {Array.from({ length: 20 }).map((_, index) => (
           <div key={index} className="shooting_star"></div>
