9f02ee6ad1ecac095f1b03ab429f24ed7ef24fb7
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-22 15:45:28 +0900

    :sparkles:환경변수 사용 및 CORS
    환경변수 사용을 위한 dotenv모듈 추가, CORS모듈 추가 및 CORS설정
    포트번호 및 클라이언트 측 url환경변수화

--- fortune-server/app/main.py
+++ fortune-server/app/main.py
@@ -1,7 +1,28 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.content import router as content_router  # content 패키지를 가져옵니다.
+from dotenv import load_dotenv
+
+load_dotenv()  # dotenv
 
 app = FastAPI()
+
+# CORS설정
+origin = os.getenv("CLIENT_URL")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origin,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World!"}
+
 
 # content 라우터 등록
 app.include_router(content_router, prefix="/content", tags=["content"])
@@ -15,4 +36,5 @@ if __name__ == "__main__":
     """
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="127.0.0.1", port=port)
