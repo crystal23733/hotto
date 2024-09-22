eee834fd6e9d154ab522a676c0c988b758b3b92f
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-22 14:25:53 +0900

    :package:코드 포매팅 및 서버 리로드 추가

--- fortune-server/app/main.py
+++ fortune-server/app/main.py
@@ -8,4 +8,5 @@ app.include_router(content_router, prefix="/content", tags=["content"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
--- fortune-server/app/routers/content/fortune/fortune.py
+++ fortune-server/app/routers/content/fortune/fortune.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+
 @router.get("/")
 async def get_fortune():
-    return {"fortune" : "오늘은 좋은일이 생길 것 같습니다!"}
+    return {"fortune": "오늘은 좋은일이 생길 것 같습니다!"}
