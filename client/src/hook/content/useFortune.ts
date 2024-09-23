29d3d3a0b8e6efe0a477adde602fd85ff4729f48
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-23 22:18:51 +0900

    :package:useFortune 커스텀 훅 수정
    응답 데이터 인터페페이스 할당
    에러 상태 변경

--- client/src/hook/content/useFortune.ts
+++ client/src/hook/content/useFortune.ts
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react";
 import useApiRequest from "../common/api/useApiRequest";
 import fortuneRequest from "client/src/api/auth/fortuneRequest";
+import FortuneResponse from "@shared/interface/fortune.interface";
 
 export default () => {
   const [contentText, setContentText] = useState<string>("");
   const { data, setData, loading, setLoading, error, setError } =
     useApiRequest();
   console.log(contentText);
-  const fortuneSubmit = async () => {
+  const fortuneSubmit = async (): Promise<FortuneResponse | null> => {
     setLoading(true);
     try {
       const result = await fortuneRequest(contentText);
       console.log(result);
       setData(result);
+      return result as FortuneResponse;
     } catch (error) {
+      setError(
+        error instanceof Error ? error : new Error("에러가 발생하였습니다."),
+      );
+      return null;
     } finally {
       setLoading(false);
     }
