51a7e36ddc64039ff2dc45f506362374f944c3b2
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-25 12:22:25 +0900

    :package:에러메세지 반환
    클라이언트 측으로 맞는 에러메세지 반환

--- fortune-server/routers/content/fortune/fortune.py
+++ fortune-server/routers/content/fortune/fortune.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Depends, HTTPException
+from fastapi import APIRouter, Request, HTTPException
 from services.content.fortune_service import generate_fortune
 from services.content.token_management import get_user, check_token_limit, update_tokens
 
@@ -16,16 +16,18 @@ async def get_fortune(request: Request):
     Returns:
         JSON: 포춘텔링 결과 반환.
     """
-    data = await request.json()
-    user_input = data.get("text")
-    session_id = request.cookies.get("session_id")
-    user = get_user(session_id)
-    if not user:
-        raise HTTPException(status_code=401, detail="사용자를 찾을 수 없습니다.")
-    check_token_limit(user)
-    print("요청 데이터:", data)  # 로그로 출력
-    fortune = await generate_fortune(user_input)
-    print("응답 데이터", fortune)
-    update_tokens(user["_id"])
-    print("사용자 정보:", user)
-    return {"fortune": fortune}
+    try:
+        data = await request.json()
+        user_input = data.get("text")
+        session_id = request.cookies.get("session_id")
+        user = get_user(session_id)
+        if not user:
+            raise HTTPException(status_code=401, detail="사용자를 찾을 수 없습니다.")
+        check_token_limit(user)
+        fortune = await generate_fortune(user_input)
+        update_tokens(user["_id"])
+        return {"fortune": fortune}
+    except HTTPException as http_ex:
+        return {"error": {"status": http_ex.status_code, "message": http_ex.detail}}
+    except Exception as e:
+        return {"error": {"status": 500, "message": str(e)}}
