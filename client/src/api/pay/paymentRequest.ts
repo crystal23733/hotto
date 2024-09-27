6c8904a7248cda02cc4d280a1cf862ed23ebf590
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-27 17:46:26 +0900

    :sparkles:백엔드 측으로 결제 요청 함수 추가

---
+++ client/src/api/pay/paymentRequest.ts
@@ -0,0 +1,9 @@
+import payServer from "client/src/module/payServer";
+import FetchApi from "../lib/FetchApi";
+
+const fetchApi = new FetchApi(payServer);
+
+export default async (option: string) => {
+  const payUrl = process.env.NEXT_PUBLIC_PAY_ENDPOINT as string;
+  return fetchApi.request(payUrl, "POST", { option }, null, true);
+};
