430715913c3e1313a7f756bf62cae2c8a0a073c8
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-28 16:50:16 +0900

    :package:결제 커스텀 훅 연결

--- client/src/api/pay/paymentRequest.ts
+++ client/src/api/pay/paymentRequest.ts
@@ -5,5 +5,6 @@ const fetchApi = new FetchApi(payServer);
 
 export default async (option: string) => {
   const payUrl = process.env.NEXT_PUBLIC_PAY_ENDPOINT as string;
+  console.log(payServer, payUrl);
   return fetchApi.request(payUrl, "POST", { option }, null, true);
 };
--- client/src/hook/common/usePaymentModal.ts
+++ client/src/hook/common/usePaymentModal.ts
@@ -1,6 +1,26 @@
+import { useEffect } from "react";
 import useApiRequest from "./api/useApiRequest";
+import paymentRequest from "client/src/api/pay/paymentRequest";
 
-export default () => {
+export default (option: string) => {
   const { data, setData, loading, setLoading, error, setError } =
     useApiRequest();
+
+  useEffect(() => {
+    const fetchPaymentData = async () => {
+      setLoading(true);
+      try {
+        const response = await paymentRequest(option);
+        setData(response);
+      } catch (error) {
+        setError(
+          error instanceof Error ? error : new Error("An Error occurred"),
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPaymentData();
+  }, [option]);
+  return { data, error, loading };
 };
