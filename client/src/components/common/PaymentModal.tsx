d0dc7c6ef1c1add568ef2baa9b00c1868bc854f0
Hoyeong Yoo <crystal23733@gmail.com>
2024-09-28 16:50:16 +0900

    :package:결제 커스텀 훅 연결

--- client/src/components/common/PaymentModal.tsx
+++ client/src/components/common/PaymentModal.tsx
@@ -3,9 +3,12 @@ import Modal from "./Modal";
 import IModalProps from "@shared/interface/modal.interface";
 import PaymentLabel from "./PaymentLabel";
 import useLottoOptions from "client/src/hook/pick/useLottoOptions";
+import usePaymentModal from "client/src/hook/common/usePaymentModal";
 
 const PaymentModal: React.FC<IModalProps> = ({ isActive, closeModal }) => {
   const { selectedOption, handleOptionChange } = useLottoOptions();
+  const { data, error, loading } = usePaymentModal(selectedOption);
+  console.log(data);
   const handlePaymentBtn = () => {};
   return (
     <Modal
@@ -13,7 +16,7 @@ const PaymentModal: React.FC<IModalProps> = ({ isActive, closeModal }) => {
       closeModal={closeModal}
       title="충전하기"
       onConfirm={handlePaymentBtn}
-      loadingStatus={false}
+      loadingStatus={loading}
     >
       <PaymentLabel
         selectedOption={selectedOption}
