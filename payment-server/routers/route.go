7bdd932d1d522ae035f7bdb0a5a172dcd3804b65
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-02 11:52:08 +0900

    :truck::recycle:라우터 정의 함수 모듈화

---
+++ payment-server/routers/route.go
@@ -0,0 +1,20 @@
+/*
+routers 패키지는 애플리케이션의 라우터를 정의하고 핸들러를 연결한다.
+*/
+
+package routers
+
+import (
+	"payment-server/controllers"
+
+	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// SetupRoutes는 Echo인스턴스에 라우트를 설정한다.
+// 각 라우트 함수는 특정 핸들러와 연결된다.
+func SetupRoutes(e *echo.Echo, client *mongo.Client) {
+	e.POST("/pay", func(c echo.Context) error {
+		return controllers.PayHandler(c, client)
+	})
+}
