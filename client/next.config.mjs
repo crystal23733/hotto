a81898089ebd01f7188fa0eec389ddaa2298bf91
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-11 14:55:57 +0900

    :package:클라이언트 측 설정 변경
    로컬 환경에서의 https접속
    개발환경 실행 시 pem파일 읽어오기
    next.config.mjs설정 변경됨

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,10 +1,27 @@
+// next.config.mjs
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-
-export default {
+const nextConfig = {
   reactStrictMode: true,
-
-  // 아래 항목 추가
   experimental: {
     externalDir: true
-  },
+  }
 };
+
+// 개발 환경에서 HTTPS 설정
+if (process.env.NODE_ENV === 'development') {
+  nextConfig.server = {
+    https: {
+      key: fs.readFileSync(path.join(__dirname, '../certs/localhost+2-key.pem')),
+      cert: fs.readFileSync(path.join(__dirname, '../certs/localhost+2.pem'))
+    }
+  };
+}
+
+export default nextConfig;
