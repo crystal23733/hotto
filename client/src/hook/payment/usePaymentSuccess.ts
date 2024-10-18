e5b76324aebc2a41144e25306e2a208ef5162456
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-18 19:06:40 +0900

    :sparkles:결제 승인 커스텀 훅 생성
    url query의 pgtoken을 이용하여 결제 승인 정보 핸들링 하는 커스텀 훅 제작

---
+++ client/src/hook/payment/usePaymentSuccess.ts
@@ -0,0 +1,36 @@
+import {useEffect, useState} from 'react';
+import FetchApi from '../../api/lib/FetchApi';
+import {useRouter} from 'next/router';
+import PaymentApprovalResponse from "@shared/interface/payment/kakaoSuccess.interface";
+import useApiRequest from '../common/api/useApiRequest';
+import payServer from "../../module/payServer";
+
+/**
+ * 결제 요청을 처리하는 커스텀 훅
+ * @returns {{data: PaymentApprovalResponse | null, loading: boolean, error: Error | null}}
+ */
+
+export default () => {
+	const {data, loading, error, setData, setLoading, setError} = useApiRequest<PaymentApprovalResponse>();
+	const router = useRouter();
+
+	useEffect(() => {
+		const pgToken = router.query.pg_token;
+		if(typeof pgToken === 'string'){
+			setLoading(true);
+			const approvalUrl = process.env.NEXT_PUBLIC_PAY_APPROVAL_ENDPOINT as string;
+			const fetchApi = new FetchApi<PaymentApprovalResponse>(payServer);
+			fetchApi.request(approvalUrl, "POST", {pg_token: pgToken})
+			.then((reponse) => {
+				setData(reponse);
+				setLoading(false);
+			}).catch((err:Error) => {
+				setError(err);
+				setLoading(false);
+			});
+		}
+	}, [router.query, setData, setError, setLoading]);
+	return {data, loading, error};
+}
+
+
