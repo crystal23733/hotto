3faf3efafe34fd509c7f7a23e8f98c1642809503
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-21 02:55:23 +0900

    :sparkles:승인 요청 본문 보내는 핸들러 추가

--- payment-server/payment/kakaopay.go
+++ payment-server/payment/kakaopay.go
@@ -9,6 +9,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -77,3 +78,50 @@ func (c *KakaoPayClient) RequestPayment(request models.KakaoPayRequest) (*models
 	log.Printf("카카오페이 응답:%+v", kakaoPayResponse)
 	return &kakaoPayResponse, nil
 }
+
+// ApprovePayment는 카카오페이 결제 승인 요청을 보냅니다.
+func (c *KakaoPayClient) ApprovePayment(pgToken, tid, orderId, userId string)(*models.KakaoPayApproveResponse, error) {
+	approveUrl := "https://open-api.kakaopay.com/online/v1/payment/approve"
+	requestBody := map[string]string{
+		"cid": c.Cid,
+		"tid": tid,
+		"partner_order_id":orderId,
+		"partner_user_id":userId,
+		"pg_token":pgToken,
+	}
+
+	jsonBody, err := json.Marshal(requestBody)
+	if err != nil {
+		log.Printf("결제 승인 요청 본문을 생성하는데에 실패하였습니다")
+		return nil, errors.New("결제 승인 요청 본문을 생성하는데에 실패하였습니다")
+	}
+
+	req, err := http.NewRequest("POST", approveUrl, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return nil, err
+	}
+	
+	req.Header.Set("Authorization", "SECRET_KEY " + c.CidSecret)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		log.Printf("카카오페이 API 호출 실패: %s, 응답 본문: %s", resp.Status, string(body))
+		return nil, fmt.Errorf("카카오페이 API 호출 실패: %s, 응답 본문: %s", resp.Status, string(body))
+	}
+
+	var approveResponse models.KakaoPayApproveResponse
+	if err := json.NewDecoder(resp.Body).Decode(&approveResponse); err != nil {
+		log.Printf("카카오페이 응답을 파싱하는데 실패하였습니다.")
+		return nil, errors.New("카카오페이 응답을 파싱하는데 실패하였습니다.")
+	}
+	return &approveResponse, nil
+}
+
