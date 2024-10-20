4b3c3a3507edc9c3f595712786f21fb405dbed9b
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-21 02:18:33 +0900

    :heavy_plus_sign:카카오페이 결제 승인 구조체 정의

--- payment-server/models/kakaoModels.go
+++ payment-server/models/kakaoModels.go
@@ -21,13 +21,49 @@ type KakaoPayRequest struct {
 
 // KakaoPayResponse는 KakaoPay로부터의 응답을 나타낸다.
 type KakaoPayResponse struct {
-	Tid string `json:"tid"` // 결제 고유 번호
-	NextRedirectAppUrl string `json:"next_redirect_app_url"` // 요청한 클라이언트가 모바일앱일경우 카카오톡결제페이지 URL
+	Tid                   string `json:"tid"`                      // 결제 고유 번호
+	NextRedirectAppUrl    string `json:"next_redirect_app_url"`    // 요청한 클라이언트가 모바일앱일경우 카카오톡결제페이지 URL
 	NextRedirectMobileUrl string `json:"next_redirect_mobile_url"` // 요청한 클라이언트가 모바일웹일경우 카카오톡결제페이지 URL
-	NextRedirectPcUrl string `json:"next_redirect_pc_url"` // 요청한 클라이언트가 PC일경우 카카오톡결제페이지 URL
-	AndroidAppScheme string `json:"android_app_scheme"` // 카카오페이 결제 화면으로 이동하는 Android 앱 스킴(Scheme) - 내부 서비스용
-	IosAppScheme string `json:"ios_app_scheme"` // 카카오페이 결제 화면으로 이동하는 iOS 앱 스킴 - 내부 서비스용
-	CreatedAt string `json:"created_at"` // 결제 준비 요청 시간
+	NextRedirectPcUrl     string `json:"next_redirect_pc_url"`     // 요청한 클라이언트가 PC일경우 카카오톡결제페이지 URL
+	AndroidAppScheme      string `json:"android_app_scheme"`       // 카카오페이 결제 화면으로 이동하는 Android 앱 스킴(Scheme) - 내부 서비스용
+	IosAppScheme          string `json:"ios_app_scheme"`           // 카카오페이 결제 화면으로 이동하는 iOS 앱 스킴 - 내부 서비스용
+	CreatedAt             string `json:"created_at"`               // 결제 준비 요청 시간
 }
 
-
+// KakaoPayApproveResponse는 KakaoPay의 결제 승인 응답을 나타낸다.
+type KakaoPayApproveResponse struct {
+	Aid               string `json:"aid"`
+	Tid               string `json:"tid"`
+	Cid               string `json:"cid"`
+	Sid               string `json:"sid"`
+	PartnerOrderId    string `json:"partner_order_id"`
+	PartnerUserId     string `json:"partner_user_id"`
+	PaymentMethodType string `json:"payment_method_type"`
+	Amount            struct {
+		Total    int `json:"total"`
+		TaxFree  int `json:"tax_free"`
+		Vat      int `json:"vat"`
+		Point    int `json:"point"`
+		Discount int `json:"discount"`
+	} `json:"amount"`
+	CardInfo struct {
+		KakaopayPurchaseCorp     string `json:"kakaopay_purchase_corp"`
+		KakaopayPurchaseCorpCode string `json:"kakaopay_purchase_corp_code"`
+		KakaopayIssuerCorp       string `json:"kakaopay_issuer_corp"`
+		KakaopayIssuerCorpCode   string `json:"kakaopay_issuer_corp_code"`
+		Bin                      string `json:"bin"`
+		CardType                 string `json:"card_type"`
+		InstallMonth             string `json:"install_month"`
+		ApprovedId               string `json:"approved_id"`
+		CardMid                  string `json:"card_mid"`
+		InterestFreeInstall      string `json:"interest_free_install"`
+		InstallmentType          string `json:"installment_type"`
+		CardItemCode             string `json:"card_item_code"`
+	}
+	ItemName   string `json:"item_name"`
+	ItemCode   string `json:"item_code"`
+	Quantity   int    `json:"quantity"`
+	CreatedAt  string `json:"created_at"`
+	ApprovedAt string `json:"approved_at"`
+	Payload    string `json:"payload"`
+}
