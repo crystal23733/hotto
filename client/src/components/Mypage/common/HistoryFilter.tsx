871271828913db5e1f6334817feb20bdca63f4b0
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-24 23:34:12 +0900

    :sparkles::art:날짜 선택 공통 컴포넌트 제작

---
+++ client/src/components/Mypage/common/HistoryFilter.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import HistoryFilterProps from "../interface/HistoryFilterProps";
+
+/**
+ * 날짜별로 내역을 필터링하는 컴포넌트
+ * @param {HistoryFilterProps} props - 날짜 필터링하는 함수
+ * @returns {JSX.Element}
+ */
+const HistoryFilter: React.FC<HistoryFilterProps> = ({ setDateFilter }) => {
+  return (
+    <div>
+      <label>날짜 선택: </label>
+      <input type="date" onChange={(e) => setDateFilter(e.target.value)} />
+    </div>
+  );
+};
+
+export default HistoryFilter;
