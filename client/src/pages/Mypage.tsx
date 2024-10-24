95f7d9a254ae055fa71f39ba1692aff923f1a3c6
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-24 17:29:48 +0900

    :art:마이페이지 탭 메뉴 렌더링
    결제내역과 주문내역에 따른 정보 렌더링

--- client/src/pages/Mypage.tsx
+++ client/src/pages/Mypage.tsx
@@ -3,9 +3,14 @@ import "../scss/myPage.scss";
 import MypageInfoContainer from "../components/Mypage/MypageInfoContainer";
 import useModal from "../hook/Modal/useMyModal";
 import MypageFormModal from "../components/Mypage/MypageFormModal";
+import useTabSwitcher from "../hook/Mypage/useTabSwitcher";
 
 const Mypage: React.FC = () => {
   const { isActive, handleMypageModal, closeModal } = useModal();
+  const { activeTab, handleTabChange } = useTabSwitcher<"payment" | "order">(
+    "payment",
+  );
+
   return (
     <div id="mypage-container">
       <div className="box" id="mypage-container__content">
@@ -13,6 +18,16 @@ const Mypage: React.FC = () => {
         <MypageFormModal isActive={isActive} closeModal={closeModal} />
         <div className="change-password-box">
           <button onClick={handleMypageModal}>비밀번호 변경</button>
+        </div>
+        <div className="tabs is-centered is-boxed">
+          <ul>
+            <li className={activeTab === "payment" ? "is-active" : ""}>
+              <a onClick={() => handleTabChange("payment")}>결제 내역</a>
+            </li>
+            <li className={activeTab === "order" ? "is-active" : ""}>
+              <a onClick={() => handleTabChange("order")}>주문 내역</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
