b635f3896e56e90263f88e133993a71ce67afb6f
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-24 12:41:38 +0900

    코드 포매팅

--- client/src/hook/Modal/usePayModal.ts
+++ client/src/hook/Modal/usePayModal.ts
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import usePaymentSecurity from "../payment/usePaymentSecurity";
 
-
 export default () => {
   const [isPayActive, setIsPayActive] = useState<boolean>(false); // * 모달창의 상태를 관리
-  const {cleanupPayment} = usePaymentSecurity();  
+  const { cleanupPayment } = usePaymentSecurity();
 
   const handlePaymentModal = () => {
     setIsPayActive(true);
