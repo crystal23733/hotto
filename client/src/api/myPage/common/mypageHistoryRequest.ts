63909b004a8954d05e12dd2c75980472c64505c0
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-24 19:15:25 +0900

    :sparkles:마이페이지 내역 동적 api 생성

---
+++ client/src/api/myPage/common/mypageHistoryRequest.ts
@@ -0,0 +1,13 @@
+/**
+ * 공통으로 데이터내역을 가져오는 api
+ * @param {string} endpoint - 엔드포인트
+ * @returns {Promise} API응답 결과
+ */
+
+import payServer from "client/src/module/payServer"
+import FetchApi from "../../lib/FetchApi"
+
+export default <T>(endpoint:string) => {
+  const fetchApi = new FetchApi<T>(payServer);
+  return fetchApi.request(endpoint, "GET");
+}
