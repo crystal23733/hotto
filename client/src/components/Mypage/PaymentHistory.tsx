572e348b1bd511ce3da10bfe3e89ec9721ac088d
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-25 11:52:54 +0900

    :package:필터링 데이터 타입 상속

--- client/src/components/Mypage/PaymentHistory.tsx
+++ client/src/components/Mypage/PaymentHistory.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HistoryFilter from "./common/HistoryFilter";
+import HistoryList from "./common/HistoryList";
 import useApiHistory from "../../hook/Mypage/useApiHistory";
 
 /**
@@ -14,6 +15,7 @@ const PaymentHistory: React.FC = () => {
   return (
     <div>
       <HistoryFilter setDateFilter={setDateFilter} />
+      <HistoryList data={data} loading={loading} error={error} />
     </div>
   );
 };
--- client/src/hook/Mypage/useApiHistory.ts
+++ client/src/hook/Mypage/useApiHistory.ts
@@ -1,6 +1,7 @@
 import mypageHistoryRequest from "client/src/api/myPage/common/mypageHistoryRequest";
 import useApiRequest from "client/src/hook/common/api/useApiRequest";
 import IHistoryItem from "../../components/Mypage/interface/IHistoryItem";
+import filterByDate from "../../utils/filter/filterByDate";
 import { useEffect, useState } from "react";
 
 /**
@@ -8,7 +9,7 @@ import { useEffect, useState } from "react";
  * @param {string} endpoint - 데이터를 가져올 API 엔드포인트
  * @returns {object} 데이터 내역, 필터링 함수, 로딩 상태, 에러 정보
  */
-export default <T>(endpoint: string) => {
+export default <T extends IHistoryItem>(endpoint: string) => {
   const { data, loading, error, setData, setLoading, setError } =
     useApiRequest<T[]>();
   const [dateFilter, setDateFilter] = useState<string>("");
@@ -30,5 +31,8 @@ export default <T>(endpoint: string) => {
     fetchHistory();
   }, []);
 
-  return { data: filterByDate(), setDateFilter, loading, error };
+  // 필터링된 데이터를 반환
+  const filteredData = filterByDate(data || [], dateFilter);
+
+  return { data: filteredData, setDateFilter, loading, error };
 };
