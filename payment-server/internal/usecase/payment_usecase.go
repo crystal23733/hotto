0d64c9064e4b5a3ffaf6fedb1c309dd2d716824c
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-30 16:53:19 +0900

    :sparkles:결제관련 usecase 생성
    컨트롤러와 비즈니스 로직을 이어주는 payment_usecase

---
+++ payment-server/internal/usecase/payment_usecase.go
@@ -0,0 +1,27 @@
+package usecase
+
+import (
+	"context"
+	"payment-server/internal/entity"
+	"payment-server/internal/repositories/mongodb"
+)
+
+// PaymentUsecase는 결제 관련 비즈니스 로직을 처리하는 유즈케이스이다.
+type PaymentUsecase struct{
+	PaymentRepo *mongodb.PaymentRepository
+}
+
+// NewPaymentUsecase는 PaymentUsecase를 생성한다.
+func NewPaymentUsecase(repo *mongodb.PaymentRepository) *PaymentUsecase{
+	return &PaymentUsecase{PaymentRepo: repo}
+}
+
+// CreatePayOrder는 결제 요청을 생성하는 유즈케이스이다.
+func (u *PaymentUsecase) CreatePayOrder(ctx context.Context, order entity.PayOrder) error {
+	return u.PaymentRepo.CreatePayOrder(ctx, order)
+}
+
+// UpdatePayOrderStatus는 결제 상태를 업데이트하는 유즈케이스이다.
+func (u *PaymentUsecase) UpdatePayOrderStatus(ctx context.Context, payOrderID string, status string) error{
+	return u.PaymentRepo.UpdatePayOrderStatus(ctx, payOrderID, status)
+}
