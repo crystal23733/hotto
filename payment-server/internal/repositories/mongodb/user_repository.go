b67d9a2c5ac96c8b952127237ed5be3c42ae1470
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-31 01:22:59 +0900

    :sparkles:user와 session을 조회하는 함수 추가

--- payment-server/internal/repositories/mongodb/user_repository.go
+++ payment-server/internal/repositories/mongodb/user_repository.go
@@ -13,13 +13,37 @@ type UserRepository struct {
 	Collection *mongo.Collection
 }
 
-func NewUserRepository (client *mongo.Client, dbName string) *UserRepository {
+func NewUserRepository(client *mongo.Client, dbName string) *UserRepository {
 	collection := client.Database(dbName).Collection("users")
 	return &UserRepository{Collection: collection}
 }
 
+// 세션 관련 레포지토리
+func NewSessionRepository(client *mongo.Client, dbName string) *UserRepository {
+	collection := client.Database(dbName).Collection("sessions")
+	return &UserRepository{Collection: collection}
+}
+
+// UserFind함수는 사용자를 조회하는 함수이다.
+func (r *UserRepository) UserFind(ctx context.Context, userID primitive.ObjectID, user interface{}) error {
+	err := r.Collection.FindOne(ctx, bson.M{"_id": userID}).Decode(user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // UpdateUserPayments는 사용자 스키마에 결제 내역을 추가한다.
-func (r *UserRepository) UpdateUserPayments (ctx context.Context, userID primitive.ObjectID, payOrderID string) error {
-	_, err := r.Collection.UpdateOne(ctx, bson.M{"_id":userID}, bson.M{"$push":bson.M{"payments":payOrderID}})
+func (r *UserRepository) UpdateUserPayments(ctx context.Context, userID primitive.ObjectID, payOrderID string) error {
+	_, err := r.Collection.UpdateOne(ctx, bson.M{"_id": userID}, bson.M{"$push": bson.M{"payments": payOrderID}})
 	return err
+}
+
+// SessionFind는 세션을 조회하는 함수이다.
+func (r *UserRepository) SessionFind(ctx context.Context, sessionID string, sessionDoc interface{}) error {
+	err := r.Collection.FindOne(ctx, bson.M{"_id": sessionID}).Decode(sessionDoc)
+	if err != nil {
+		return err
+	}
+	return nil
 }
