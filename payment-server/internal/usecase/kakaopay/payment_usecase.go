9b7aac82376becbeb79b2aa88721ec992b0eff32
Hoyeong Yoo <crystal23733@gmail.com>
2024-10-30 17:35:04 +0900

    :package:payment_usecase코드 변경
    Kakaopay 서비스 로직 추가

--- payment-server/internal/usecase/kakaopay/payment_usecase.go
+++ payment-server/internal/usecase/kakaopay/payment_usecase.go
@@ -1,19 +1,21 @@
-package usecase
+package kakaopay
 
 import (
 	"context"
 	"payment-server/internal/entity"
 	"payment-server/internal/repositories/mongodb"
+	"payment-server/internal/services/kakaopay"
 )
 
 // PaymentUsecase는 결제 관련 비즈니스 로직을 처리하는 유즈케이스이다.
 type PaymentUsecase struct{
 	PaymentRepo *mongodb.PaymentRepository
+	KakaoPayService *kakaopay.KakaoPayService
 }
 
 // NewPaymentUsecase는 PaymentUsecase를 생성한다.
-func NewPaymentUsecase(repo *mongodb.PaymentRepository) *PaymentUsecase{
-	return &PaymentUsecase{PaymentRepo: repo}
+func NewPaymentUsecase(repo *mongodb.PaymentRepository, kakaoService *kakaopay.KakaoPayService) *PaymentUsecase{
+	return &PaymentUsecase{PaymentRepo: repo, KakaoPayService: kakaoService}
 }
 
 // CreatePayOrder는 결제 요청을 생성하는 유즈케이스이다.
