c28720e5a38d34571f9600cc33dbdf0542f5f318
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-04 17:01:42 +0900

    :package:클라이언트 측 메서드 변경 및 라우터 추가
    클라이언트 측 요청함수 POST -> GET, Cookie 전달 true
    백엔드 측 라우팅 POST -> GET 변경

--- payment-server/internal/routers/router.go
+++ payment-server/internal/routers/router.go
@@ -6,10 +6,12 @@ package routers
 
 import (
 	"payment-server/internal/config"
-	controllers "payment-server/internal/controllers/kakaopay"
+	kakaoControllers "payment-server/internal/controllers/kakaopay"
+	queryControllers "payment-server/internal/controllers/lookup"
 	"payment-server/internal/repositories/mongodb"
 	"payment-server/internal/services/kakaopay"
-	usecase "payment-server/internal/usecase/kakaopay"
+	kakaoUsecase "payment-server/internal/usecase/kakaopay"
+	queryUsecase "payment-server/internal/usecase/lookup"
 
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -28,12 +30,18 @@ func SetupRoutes(e *echo.Echo, client *mongo.Client) {
 	kakaoService := kakaopay.NewKakaoPayService()
 
 	// 유즈케이스 설정
-	paymentUsecase := usecase.NewPaymentUsecase(paymentRepo, userRepo, sessionRepo, kakaoService)
+	paymentUsecase := kakaoUsecase.NewPaymentUsecase(paymentRepo, userRepo, sessionRepo, kakaoService)
 	paymentUsecase.UserRepo = userRepo
 
 	// 핸들러 설정
-	kakaoPayHandler := controllers.NewPaymentHandler(paymentUsecase)
+	kakaoPayHandler := kakaoControllers.NewPaymentHandler(paymentUsecase)
 
 	e.POST("/pay", kakaoPayHandler.CreatePayment)
 	e.POST("/pay/approval", kakaoPayHandler.PayApproveHandler)
+
+	// 결제 조회 관련 라우팅 설정
+	paymentQueryUsecase := queryUsecase.NewPaymentQueryUsecase(paymentRepo, userRepo, sessionRepo)
+	paymentQueryHandler := queryControllers.NewPaymentQueryHandler(paymentQueryUsecase)
+
+	e.GET("/payment-history", paymentQueryHandler.GetPayments)
 }
