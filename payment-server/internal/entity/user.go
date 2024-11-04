011ca7de84389429c905de376eee2d6f75b42995
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-04 11:51:06 +0900

    :package:user 구조체 변경
    결제 내역 추가에 따른 변경

--- payment-server/internal/entity/user.go
+++ payment-server/internal/entity/user.go
@@ -5,10 +5,13 @@ User 구조체는 사용자의 정의를 나타낸다.
 
 package entity
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 // User는 사용자의 정보를 나타내는 구조체이다.
 type User struct {
-	ID      string `json:"_id" bson:"_id"`
-	Name    string `json:"name" bson:"name"`
-	Email   string `json:"email" bson:"email"`
-	Balance int    `json:"balance" bson:"balance"`
+	ID       string               `json:"_id" bson:"_id"`
+	Name     string               `json:"name" bson:"name"`
+	Email    string               `json:"email" bson:"email"`
+	Balance  int                  `json:"balance" bson:"balance"`
+	Payments []primitive.ObjectID `json:"payments" bson:"payments"`
 }
