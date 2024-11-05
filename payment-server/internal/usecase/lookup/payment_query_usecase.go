5ca00facb7814b39f915a9bda155a512f80e8ee1
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-05 12:32:56 +0900

    :package:결제 조회 시 한국 기준 시간 변경

--- payment-server/internal/usecase/lookup/payment_query_usecase.go
+++ payment-server/internal/usecase/lookup/payment_query_usecase.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"payment-server/internal/entity"
 	"payment-server/internal/repositories/mongodb"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -23,7 +24,7 @@ func NewPaymentQueryUsecase(PaymentRepo *mongodb.PaymentRepository, UserRepo *mo
 }
 
 // GetUserPayments는 사용자 ID를 기반으로 결제 정보를 조회하는 유즈케이스이다.
-func (u *PaymentQueryUsecase) GetUserPayments(ctx context.Context, userID primitive.ObjectID) ([]entity.PayOrder, error) {
+func (u *PaymentQueryUsecase) GetUserPayments(ctx context.Context, userID primitive.ObjectID) ([]entity.PayOrderResponse, error) {
 	var user entity.User
 	// 사용자 조회
 	err := u.UserRepo.UserFind(ctx, userID, &user)
@@ -34,7 +35,7 @@ func (u *PaymentQueryUsecase) GetUserPayments(ctx context.Context, userID primit
 
 	// 결제 내역 조회
 	paymentIDs := user.Payments
-	var payments []entity.PayOrder
+	var payments []entity.PayOrderResponse
 	for _, paymentID := range paymentIDs {
 		var payOrder entity.PayOrder
 		err := u.PaymentRepo.FindPayOrderByObjectID(ctx, paymentID, &payOrder)
@@ -42,7 +43,24 @@ func (u *PaymentQueryUsecase) GetUserPayments(ctx context.Context, userID primit
 			log.Printf("결제 내역 조회 실패 (결제 ID: %v): %v", paymentID.Hex(), err)
 			continue // 조회 실패 시 계속 진행
 		}
-		payments = append(payments, payOrder)
+
+		// created_at을 KST로 변환
+		kst, _ := time.LoadLocation("Asia/Seoul")
+		createdAtKST := payOrder.CreatedAt.In(kst)
+
+		// 필요한 형식으로 변환 (예: "YYYY-MM-DD HH:mm")
+		createdAtFormatted := createdAtKST.Format("2006년 1월 2일 15:04")
+
+		// 필요한 필드만 담은 PayOrderResponse 생성
+		payOrderResponse := entity.PayOrderResponse{
+			PayOrderID: payOrder.PayOrderID,
+			Amount:     payOrder.Amount,
+			Status:     payOrder.Status,
+			CreatedAt:  createdAtFormatted,
+			Pay:        payOrder.Pay,
+		}
+
+		payments = append(payments, payOrderResponse)
 	}
 
 	if len(payments) == 0 {
