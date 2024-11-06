405380b18dc0c7c54688b6503dd31e1a787b1efb
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-06 15:17:27 +0900

    :package:로직 커스텀 훅 추가
    특정 로직으로 번호를 생성할 경우 서버 측 요청 추가

---
+++ client/src/api/content/pick/buyNumberRequest.ts
@@ -0,0 +1,14 @@
+import payServer from "client/src/module/payServer";
+import FetchApi from "../../lib/FetchApi";
+import {
+  IPaymentRequest,
+  IPaymentResponse,
+} from "client/src/pipes/interface/pick/buyNumber.interface";
+
+const fetchApi = new FetchApi<IPaymentResponse>(payServer);
+
+export default async (body: IPaymentRequest): Promise<IPaymentResponse> => {
+  const buyNumberUrl = process.env
+    .NEXT_PUBLIC_PAY_BUY_NUMBER_ENDPOINT as string;
+  return await fetchApi.request(buyNumberUrl, "POST", body, null, true);
+};
