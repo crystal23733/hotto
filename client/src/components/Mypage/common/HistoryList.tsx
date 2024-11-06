24c3912f2ca531e26467efea18e7c24600516856
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-06 10:53:44 +0900

    :package:내역 무한 스크롤 기능 추가

--- client/src/components/Mypage/common/HistoryList.tsx
+++ client/src/components/Mypage/common/HistoryList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import HistoryListProps from "../interface/HistoryListProps";
 import Loading from "../../common/Loading";
 
@@ -8,6 +8,41 @@ import Loading from "../../common/Loading";
  * @returns {JSX.Element} 컴포넌트
  */
 const HistoryList: React.FC<HistoryListProps> = ({ data, loading, error }) => {
+  const [visibleItems, setVisibleItems] = useState(5);
+  const observer = useRef<IntersectionObserver | null>(null);
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    // 모든 데이터를 이미 보여주고 있다면 observer를 설정하지 않음
+    if (data && visibleItems >= data.length) {
+      if (observer.current) observer.current.disconnect();
+      return;
+    }
+
+    if (observer.current) observer.current.disconnect();
+
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && !loading && data) {
+          setVisibleItems((prev) => Math.min(prev + 5, data.length));
+        }
+      },
+      {
+        root: null,
+        rootMargin: "20px",
+        threshold: 0.1,
+      },
+    );
+
+    if (loadMoreRef.current) {
+      observer.current.observe(loadMoreRef.current);
+    }
+
+    return () => {
+      if (observer.current) observer.current.disconnect();
+    };
+  }, [visibleItems, data, loading]); // 의존성 배열에 visibleItems, data, loading 추가
+
   if (!data || data.length === 0) {
     return (
       <div className="empty-state history-error">
@@ -18,17 +53,20 @@ const HistoryList: React.FC<HistoryListProps> = ({ data, loading, error }) => {
 
   return (
     <div className="history-list">
-      {data.map((item) => (
+      {data.slice(0, visibleItems).map((item) => (
         <div className="history-item" key={item.pay_order_id}>
           <p className="history-item-title">주문번호: {item.pay_order_id}</p>
           <p className="history-item-amount">상품 금액: {item.amount}</p>
           <p className="history-item-detail">결제 상태: {item.status}</p>
           <p className="history-item-detail">결제 방법: {item.pay}</p>
           <p className="history-item-date">결제일: {item.created_at}</p>
-          {loading && <Loading />}
-          {error && <p className="help is-danger">{error.message}</p>}
         </div>
       ))}
+      {loading && <Loading />}
+      {error && <p className="help is-danger">{error.message}</p>}
+      {visibleItems < data.length && (
+        <div ref={loadMoreRef} style={{ height: "20px" }} />
+      )}
     </div>
   );
 };
