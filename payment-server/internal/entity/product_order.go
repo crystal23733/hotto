9bacc7e476a8b191122c0f0e6458ba6a8d6a2625
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-08 11:49:09 +0900

    :package:상품 주문내역 구조체 변경
    구매한 번호 조합 추가

--- payment-server/internal/entity/product_order.go
+++ payment-server/internal/entity/product_order.go
@@ -8,9 +8,10 @@ import (
 
 // ProductOrder는 상품의 결제를 정의하는 구조체이다.
 type ProductOrder struct {
-	PayOrderID string             `json:"pay_order_id" bson:"pay_order_id"`
-	UserID     primitive.ObjectID `json:"user_id" bson:"user_id"`
-	Amount     int                `json:"amount" bson:"amount"`
-	Status     string             `json:"status" bson:"status"`
-	CreatedAt  time.Time          `json:"created_at" bson:"created_at"`
+	PayOrderID   string             `json:"pay_order_id" bson:"pay_order_id"`
+	UserID       primitive.ObjectID `json:"user_id" bson:"user_id"`
+	Amount       int                `json:"amount" bson:"amount"`
+	Status       string             `json:"status" bson:"status"`
+	CreatedAt    time.Time          `json:"created_at" bson:"created_at"`
+	LottoNumbers []int              `json:"lotto_numbers" bson:"lotto_numbers"`
 }
