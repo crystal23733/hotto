8f7efee5f05506c984ccfb5b899b918a1b0b16e0
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-08 17:33:17 +0900

    :sparkles:유료 번호 생성을 담당하는 유스케이스 제작

--- payment-server/internal/usecase/order/order_usecase.go
+++ payment-server/internal/usecase/order/order_usecase.go
@@ -1,17 +1,106 @@
 package usecase
 
-import "payment-server/internal/repositories/mongodb"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"payment-server/internal/entity"
+	"payment-server/internal/repositories/mongodb"
+	usecase "payment-server/internal/usecase/lotto"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 // OrderUsecase는 상품 결제 관련 로직을 담당하는 유즈케이스이다.
 type OrderUsecase struct {
-	UserRepo    *mongodb.UserRepository
-	SessionRepo *mongodb.SessionRepository
+	UserRepo     *mongodb.UserRepository
+	SessionRepo  *mongodb.SessionRepository
+	OrderRepo    *mongodb.OrderRepository
+	LottoUsecase usecase.LottoUsecase
+	Client       *mongo.Client
 }
 
 // NewOrderUsecase는 OrderUsecase를 초기화한다.
-func NewOrderUsecase(userRepo *mongodb.UserRepository, sessionRepo *mongodb.SessionRepository) *OrderUsecase {
+func NewOrderUsecase(userRepo *mongodb.UserRepository, sessionRepo *mongodb.SessionRepository, orderRepo *mongodb.OrderRepository, lottoUsecase usecase.LottoUsecase, client *mongo.Client) *OrderUsecase {
 	return &OrderUsecase{
-		UserRepo:    userRepo,
-		SessionRepo: sessionRepo,
+		UserRepo:     userRepo,
+		SessionRepo:  sessionRepo,
+		LottoUsecase: lottoUsecase,
+		Client:       client,
 	}
+}
+
+func (u *OrderUsecase) CreateProductOrder(ctx context.Context, userId string, request *entity.ProdctOrderRequest) ([]int, error) {
+	// 트랜잭션 생성
+	sess, err := u.Client.StartSession()
+	if err != nil {
+		log.Printf("트랜잭션 생성에 실패하였습니다:%v", err)
+		return nil, fmt.Errorf("트랜잭션을 생성하지 못했습니다: %w", err)
+	}
+	defer sess.EndSession(ctx)
+
+	var lottoNumbers []int
+
+	err = mongo.WithSession(ctx, sess, func(sc mongo.SessionContext) error {
+		var user entity.User
+		objectID, err := primitive.ObjectIDFromHex(userId)
+		if err != nil {
+			return fmt.Errorf("유효하지 않은 ID입니다: %w", err)
+		}
+		log.Printf("objectID로 변환된 userId : %s", objectID)
+
+		err = u.UserRepo.UserFind(sc, objectID, &user)
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				return fmt.Errorf("사용자를 찾을 수 없습니다: %w", err)
+			}
+			return fmt.Errorf("데이터 베이스 오류가 발생하였습니다: %w", err)
+		}
+
+		if user.Balance < request.Amount {
+			return errors.New("잔액이 부족합니다")
+		}
+
+		// 번호 생성
+		lottoNumbers, err = u.LottoUsecase.GenerateUniqueNumbers()
+		if err != nil {
+			return fmt.Errorf("번호 조합에 실패하였습니다: %w", err)
+		}
+
+		// 주문 생성
+		newOrder := &entity.ProductOrder{
+			PayOrderID:   request.PayOrderID,
+			UserID:       objectID,
+			Amount:       request.Amount,
+			Status:       "결제 완료",
+			CreatedAt:    request.CreatedAt,
+			LottoNumbers: lottoNumbers,
+		}
+
+		err = u.OrderRepo.CreateOrder(sc, newOrder)
+		if err != nil {
+			return fmt.Errorf("주문 저장 중 오류가 발생했습니다: %w", err)
+		}
+
+		// 사용자 잔액 감소 및 주문내역 업데이트
+		update := bson.M{
+			"$inc":  bson.M{"balance": -request.Amount},
+			"$push": bson.M{"orders": newOrder.PayOrderID},
+		}
+		_, err = u.UserRepo.Collection.UpdateOne(sc, bson.M{"_id": userId}, update)
+		if err != nil {
+			return fmt.Errorf("사용자 잔액 업데이트 중 오류가 발생했습니다: %w", err)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("주문 생성 트랜잭션 중 오류가 발생했습니다: %w", err)
+	}
+
+	return lottoNumbers, nil
 }
