5af278cb5be46b31b679cbe41382a59d4d8f823b
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-08 15:17:14 +0900

    :package:상품 결제 요청 구조체 생성

--- payment-server/internal/entity/product_order.go
+++ payment-server/internal/entity/product_order.go
@@ -15,3 +15,10 @@ type ProductOrder struct {
 	CreatedAt    time.Time          `json:"created_at" bson:"created_at"`
 	LottoNumbers []int              `json:"lotto_numbers" bson:"lotto_numbers"`
 }
+
+// ProductOrderRequest는 클라이언트 측에서 요청한 데이터의 구조체이다.
+type ProdctOrderRequest struct {
+	PayOrderID string    `json:"pay_order_id"`
+	Amount     int       `json:"amount"`
+	CreatedAt  time.Time `json:"created_at"`
+}
