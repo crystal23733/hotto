28c063b89488560a5031f026401d47c7dc203a97
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-10 02:36:21 +0900

    :package:요청 경로 수정

--- client/src/api/content/pick/buyNumberRequest.ts
+++ client/src/api/content/pick/buyNumberRequest.ts
@@ -8,7 +8,14 @@ import {
 const fetchApi = new FetchApi<IPaymentResponse>(payServer);
 
 export default async (body: IPaymentRequest): Promise<IPaymentResponse> => {
+  const apiUrl = process.env.NEXT_PUBLIC_API_ENDPOINT as string;
   const buyNumberUrl = process.env
     .NEXT_PUBLIC_PAY_BUY_NUMBER_ENDPOINT as string;
-  return await fetchApi.request(buyNumberUrl, "POST", body, null, true);
+  return await fetchApi.request(
+    apiUrl + buyNumberUrl,
+    "POST",
+    body,
+    null,
+    true,
+  );
 };
