a1e194d0fa4b5c96df17238dedcd40143d7f1efe
Hoyeong Yoo <crystal23733@gmail.com>
2024-11-14 15:23:35 +0900

    :package:비밀번호 변경 시 세션 확인 모달
    비밀번호 변경 시 401에러 반환 시 세션 확인 새로고침 모달창 제작

--- client/src/components/Mypage/MypageFormModal.tsx
+++ client/src/components/Mypage/MypageFormModal.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import IModalProps from "@shared/interface/modal.interface";
 import useChangePassword from "client/src/hook/Mypage/useChangePassword";
 import Modal from "../common/modal/Modal";
+import SessionExpiredModal from "../common/modal/SessionExpiredModal";
 
 const MypageFormModal: React.FC<IModalProps> = ({ isActive, closeModal }) => {
   const {
@@ -15,6 +16,8 @@ const MypageFormModal: React.FC<IModalProps> = ({ isActive, closeModal }) => {
     loading,
     error,
     setError,
+    isSessionExpired,
+    setIsSessionExpired,
   } = useChangePassword();
 
   const handleChangePasswordBtn = async () => {
@@ -24,37 +27,49 @@ const MypageFormModal: React.FC<IModalProps> = ({ isActive, closeModal }) => {
       setError(null);
     }
   };
+
+  const handleSessionExpiredConfirm = () => {
+    setIsSessionExpired(false);
+    window.location.reload(); // 확인 버튼을 누르면 페이지 새로고침
+  };
+
   return (
-    <Modal
-      isActive={isActive}
-      closeModal={closeModal}
-      title="비밀번호 변경"
-      onConfirm={handleChangePasswordBtn}
-      loadingStatus={loading}
-    >
-      <input
-        type="password"
-        className="input"
-        placeholder="기존 비밀번호"
-        value={oldPassword}
-        onChange={(event) => setOldPassword(event.target.value)}
+    <>
+      <Modal
+        isActive={isActive}
+        closeModal={closeModal}
+        title="비밀번호 변경"
+        onConfirm={handleChangePasswordBtn}
+        loadingStatus={loading}
+      >
+        <input
+          type="password"
+          className="input"
+          placeholder="기존 비밀번호"
+          value={oldPassword}
+          onChange={(event) => setOldPassword(event.target.value)}
+        />
+        <input
+          type="password"
+          className="input"
+          placeholder="변경할 비밀번호"
+          value={changePassword}
+          onChange={(event) => setChangePassword(event.target.value)}
+        />
+        <input
+          type="password"
+          className="input"
+          placeholder="변경할 비밀번호 확인"
+          onChange={(event) => setChangePasswordConfirm(event.target.value)}
+          value={changePasswordConfirm}
+        />
+        {error && <p className="help is-danger">{error.message}</p>}
+      </Modal>
+      <SessionExpiredModal
+        isVisible={isSessionExpired}
+        onConfirm={handleSessionExpiredConfirm}
       />
-      <input
-        type="password"
-        className="input"
-        placeholder="변경할 비밀번호"
-        value={changePassword}
-        onChange={(event) => setChangePassword(event.target.value)}
-      />
-      <input
-        type="password"
-        className="input"
-        placeholder="변경할 비밀번호 확인"
-        onChange={(event) => setChangePasswordConfirm(event.target.value)}
-        value={changePasswordConfirm}
-      />
-      {error && <p className="help is-danger">{error.message}</p>}
-    </Modal>
+    </>
   );
 };
 
